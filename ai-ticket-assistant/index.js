66558d02343d91d0c2e8a0af118c1f3aab839e86
Pratham Dangaich <prathamdangaich@gmail.com>
2025-08-20 20:19:59 +0530

    index.js file

---
+++ ai-ticket-assistant/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then( () => {
+        console.log("MongoDB connected ✅")
+        app.listen(PORT, () => console.log(`Server connected at port ${PORT}`))
+    })
+    .catch( (err) => console.error("❌ MongoDB error: ", err) );
+
