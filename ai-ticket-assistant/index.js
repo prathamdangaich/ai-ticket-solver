509255f117b438e5103453ccea4ac99b92abe5a1
Pratham Dangaich <prathamdangaich@gmail.com>
2025-08-23 10:57:13 +0530

    ticket controller and routes

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -1,7 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+import {serve} from "inngest/express"
 import userRoutes from "./routes/user.js"
+import ticketRoutes from "./routes/ticket.js"
+import {inngest} from "./inngest/client.js"
+import {onUserSignup} from "./inngest/functions/on-signup.js"
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js"
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,7 +14,13 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/auth", userRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/ticket",ticketRoutes);
+
+app.use("/api/inngest",serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated]
+}));
 
 mongoose
     .connect(process.env.MONGO_URI)
